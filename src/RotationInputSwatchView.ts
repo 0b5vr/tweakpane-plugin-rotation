d62e83fc04751b7029695e3e8e237f21bbf4b8d5
0b5vr <0b5vr@0b5vr.com>
2022-02-06 17:55:02 +0900

    ux: update swatch view

--- src/RotationInputSwatchView.ts
+++ src/RotationInputSwatchView.ts
@@ -1,11 +1,19 @@
 import { ClassName, SVG_NS, Value, View } from '@tweakpane/core';
 import { PointProjector } from './PointProjector';
+import { Quaternion } from './Quaternion';
 import { Rotation } from './Rotation';
 import { SVGLineStrip } from './SVGLineStrip';
+import { Vector3 } from './Vector3';
+import { createArcRotation } from './createArcRotation';
 import { createArcVerticesArray } from './createArcVerticesArray';
 import type { RotationInputSwatchViewConfig } from './RotationInputSwatchViewConfig';
 
 const className = ClassName( 'rotationswatch' );
+
+const VEC3_XP = new Vector3( 1.0, 0.0, 0.0 );
+const VEC3_YP = new Vector3( 0.0, 1.0, 0.0 );
+const VEC3_ZP = new Vector3( 0.0, 0.0, 1.0 );
+const QUAT_IDENTITY = new Quaternion( 0.0, 0.0, 0.0, 1.0 );
 
 export class RotationInputSwatchView implements View {
   public readonly element: HTMLElement;
@@ -16,6 +24,7 @@ export class RotationInputSwatchView implements View {
   private readonly xArc_: SVGLineStrip;
   private readonly yArc_: SVGLineStrip;
   private readonly zArc_: SVGLineStrip;
+  private readonly rArc_: SVGLineStrip;
 
   public constructor( doc: Document, config: RotationInputSwatchViewConfig ) {
     this.onValueChange_ = this.onValueChange_.bind( this );
@@ -41,19 +50,24 @@ export class RotationInputSwatchView implements View {
     this.projector_ = new PointProjector();
     this.projector_.viewport = [ 0, 0, 20, 20 ];
 
+    const arcArray = createArcVerticesArray( 0.0, Math.PI, 33, 'x', 'y' );
+    const arcArrayR = createArcVerticesArray( 0.0, 2.0 * Math.PI, 65, 'x', 'y' );
+
     // arc
-    const arcArrayX = createArcVerticesArray( 0.0, 2.0 * Math.PI, 65, 'y', 'z' );
-    this.xArc_ = new SVGLineStrip( doc, arcArrayX, this.projector_ );
+    this.rArc_ = new SVGLineStrip( doc, arcArrayR, this.projector_ );
+    this.rArc_.element.classList.add( className( 'arcr' ) );
+    svgElem.appendChild( this.rArc_.element );
+    this.rArc_.setRotation( QUAT_IDENTITY );
+
+    this.xArc_ = new SVGLineStrip( doc, arcArray, this.projector_ );
     this.xArc_.element.classList.add( className( 'arc' ) );
     svgElem.appendChild( this.xArc_.element );
 
-    const arcArrayY = createArcVerticesArray( 0.0, 2.0 * Math.PI, 65, 'x', 'z' );
-    this.yArc_ = new SVGLineStrip( doc, arcArrayY, this.projector_ );
+    this.yArc_ = new SVGLineStrip( doc, arcArray, this.projector_ );
     this.yArc_.element.classList.add( className( 'arc' ) );
     svgElem.appendChild( this.yArc_.element );
 
-    const arcArrayZ = createArcVerticesArray( 0.0, 2.0 * Math.PI, 65, 'x', 'y' );
-    this.zArc_ = new SVGLineStrip( doc, arcArrayZ, this.projector_ );
+    this.zArc_ = new SVGLineStrip( doc, arcArray, this.projector_ );
     this.zArc_.element.classList.add( className( 'arc' ) );
     svgElem.appendChild( this.zArc_.element );
 
@@ -64,9 +78,13 @@ export class RotationInputSwatchView implements View {
     const q = this.value.rawValue.quat.normalized;
 
     // rotate axes
-    this.xArc_.setRotation( q );
-    this.yArc_.setRotation( q );
-    this.zArc_.setRotation( q );
+    const xp = VEC3_XP.applyQuaternion( q );
+    const yp = VEC3_YP.applyQuaternion( q );
+    const zp = VEC3_ZP.applyQuaternion( q );
+
+    this.xArc_.setRotation( createArcRotation( xp, VEC3_ZP ) );
+    this.yArc_.setRotation( createArcRotation( yp, VEC3_ZP ) );
+    this.zArc_.setRotation( createArcRotation( zp, VEC3_ZP ) );
   }
 
   private onValueChange_(): void {
