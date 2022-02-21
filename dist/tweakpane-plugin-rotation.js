48815bf6131afca2f9220de85d99d663d3c262c4
0b5vr <0b5vr@users.noreply.github.com>
2022-02-21 08:23:59 +0000

    deploy: bf71a67ff950f4cef8a42b7c5958d5f97812ea99

--- dist/tweakpane-plugin-rotation.js
+++ dist/tweakpane-plugin-rotation.js
@@ -1571,7 +1571,7 @@
 
     class PointProjector {
         constructor() {
-            this.offset = [0.0, 0.0, 5.0];
+            this.offset = [0.0, 0.0, -5.0];
             this.fov = 30.0;
             this.aspect = 1.0;
             this.viewport = [0, 0, 1, 1];
@@ -1582,7 +1582,7 @@
             const vw = (this.viewport[2] - this.viewport[0]);
             const vh = (this.viewport[3] - this.viewport[1]);
             const p = 1.0 / Math.tan(this.fov * Math.PI / 360.0);
-            const sz = v.z + this.offset[2];
+            const sz = -(v.z + this.offset[2]);
             const sx = vcx + (v.x + this.offset[0]) / sz * p * vw * 0.5 / this.aspect;
             const sy = vcy - (v.y + this.offset[1]) / sz * p * vh * 0.5;
             return [sx, sy];
